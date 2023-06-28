6a47fd84257e3b8db926dcfbb3ac548d10762ee9
3782291211 <Laklouk98@hotmail.com>
2023-06-28 23:29:17 +0100

    Adding watcher and mounted hook to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
-import { computed, nextTick, onUnmounted, ref, Ref, watch } from 'vue';
+import { computed, onMounted, ref, Ref, watch } from 'vue';
 import { searchSubreddits } from '@/utils/apiRequests';
+import { fetchPopularSubreddits } from '../utils/apiRequests';
+import { useRoute } from 'vue-router';
 
 const searchResults = ref([]);
+const popularSubreddits: Ref<any> = ref([]);
 const searchTerm: Ref<string> = ref('');
 const isLoading: Ref<boolean> = ref(false);
 const inputIsBlurred: Ref<boolean> = ref(true);
+const activeParam: Ref<string> = ref('');
+const route = useRoute();
 
-// Trigger a new search with each update to the search field
+onMounted(async () => {
+  popularSubreddits.value = await fetchPopularSubreddits();
+});
+
 watch([searchTerm, inputIsBlurred], async ([newSearchTerm]) => {
     if (inputIsBlurred.value) return;
     isLoading.value = true;
@@ -15,17 +23,25 @@ watch([searchTerm, inputIsBlurred], async ([newSearchTerm]) => {
     isLoading.value = false;
 });
 
+watch(() => route.params, toParam => {
+  activeParam.value = toParam.subreddit as string || '';
+})
+
 const noResults = computed(() => {
-    return searchTerm.value.trim() 
+    return searchTerm.value 
     && !searchResults.value.length 
     && !inputIsBlurred.value;
+})
+
+const showSearchResults = computed(() => {
+  return searchResults.value.length && !inputIsBlurred.value;
 })
 
 const handleBlur = (e: FocusEvent) => {
   const targetName = (e.relatedTarget as any).name;
   if (targetName === "link") return;
   inputIsBlurred.value = true;
-  searchResults.value = [];
+ // searchResults.value = [];
 }
 
 const handleFocus = () => inputIsBlurred.value = false;
@@ -35,14 +51,21 @@ const handleFocus = () => inputIsBlurred.value = false;
 <template>
   <section id="feeds">
     <label for="search">Search subreddits</label>  
-    <input id="search" v-model="searchTerm" @blur="handleBlur" @focus="handleFocus">
-    <ul v-if="searchResults.length">
+    <input id="search" v-model.trim="searchTerm" @blur="handleBlur" @focus="handleFocus">
+    <ul v-if="showSearchResults">
       <li v-for="subreddit in searchResults">
         <a name="link" :href="`/${subreddit}`"><p class="search-results">{{ subreddit }}</p></a>
       </li>
     </uL>
     <p v-else-if="noResults">No results.</p>
+    <template v-if="!showSearchResults">
     <h3>Popular subreddits</h3>
+      <ul>
+        <li v-for="subreddit in popularSubreddits" :key="subreddit">
+          <router-link :class="[{ current : subreddit.slice(2) === activeParam }, 'popular']" :to="`/${subreddit}`">{{ subreddit }}</router-link>
+        </li>
+      </ul>
+    </template>
   </section>
 </template>
 
@@ -51,7 +74,7 @@ const handleFocus = () => inputIsBlurred.value = false;
   width: 150px;
   position: fixed;
   top: 20%;
-  left: 1%
+  left: 1%;
 }
 
 input {
@@ -76,10 +99,19 @@ ul {
     text-overflow: ellipsis;
     padding: 2px 5px
 }
-.search-results:hover {
+.search-results:hover, .popular:hover, .current {
     background-color: rgb(255, 82, 3);
     color: rgb(255, 255, 255);
     cursor: pointer;
+}
+
+.popular {
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 2px 6px;
+  transition: 0.4s;
+  border-radius: 4px
 }
 
 @media (max-width: 1000px) {
@@ -91,6 +123,14 @@ ul {
 @media (max-width: 1200px) {
   #feeds {
     left: 1%;
+  }
+}
+
+@media (hover: hover) {
+  li:hover {
+    background-color: rgb(251, 200, 92);
+    border-radius: 4px;
+    color: black;
   }
 }
 
