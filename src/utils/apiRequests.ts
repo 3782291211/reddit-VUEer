f85cfee87850b1fcdecf36942ebe7036ecc44be0
3782291211 <Laklouk98@hotmail.com>
2023-06-28 23:28:16 +0100

    Adding function to fetch popular threads

--- src/utils/apiRequests.ts
+++ src/utils/apiRequests.ts
@@ -1,18 +1,11 @@
-export const fetchPopularThreads = async ([after, before, count]: PopularThreadsArgs): Promise<PopularThreadsResponse> => {
-  const baseUrl = 'https://www.reddit.com/r/popular.json';
-  //const baseUrl = 'https://www.reddit.com/r/all/top.json';
-  const queryString = !after && !before ? ''
-  : !after && before ? `?before=${before}&count=${count}`
-  : after && !before ? `?after=${after}&count=${count}`
-  : '';
-  const response: Response = await fetch(baseUrl + queryString);
-  const json: PopularJsonResponse = await response.json();
-  const popularThreads: Subreddit[] = json.data.children.map((sub: Subreddit) => {
+const transformJsonResponse = (json: any) => {
+  return json.data.children.map((sub: Subreddit) => {
     return {
+      id: sub.data.id,
       subreddit: 'r/' + sub.data.subreddit,
       created: new Date(sub.data.created).toDateString(),
       url: sub.data.permalink.slice(0, -1),
-      body: sub.data.selftext,
+      selftext: sub.data.selftext_html,
       title: sub.data.title,
       author: sub.data.author,
       ups: sub.data.ups,
@@ -23,7 +16,18 @@ export const fetchPopularThreads = async ([after, before, count]: PopularThreads
       media: sub.data.secure_media?.reddit_video?.fallback_url || null
     };
   });
+}
 
+export const fetchPopularThreads = async ([after, before, count]: PopularThreadsArgs): Promise<PopularThreadsResponse> => {
+  const baseUrl = 'https://www.reddit.com/r/popular.json';
+  //const baseUrl = 'https://www.reddit.com/r/all/top.json';
+  const queryString = !after && !before ? ''
+  : !after && before ? `?before=${before}&count=${count}`
+  : after && !before ? `?after=${after}&count=${count}`
+  : '';
+  const response: Response = await fetch(baseUrl + queryString);
+  const json: PopularJsonResponse = await response.json();
+  const popularThreads: Subreddit[] = transformJsonResponse(json);
   return { 
     popularThreads,
     pagination : {
@@ -37,13 +41,13 @@ export const fetchPopularThreads = async ([after, before, count]: PopularThreads
 export const fetchSingleThread = async (subreddit: string, id: string, threadTitle: string) => {
   const response = await fetch(`https://www.reddit.com/r/${subreddit}/comments/${id}/${threadTitle}.json`);
   const json = await response.json();
-  const { author, title, ups, downs, selftext } = json[0].data.children[0].data;
+  const { author, title, ups, downs, selftext_html: selftext } = json[0].data.children[0].data;
   const comments = json[1].data.children.map(({ data }: { data: any }) => {
     return {
       id: data.id,
       title: data.title,
       author: data.author,
-      body: data.body,
+      body: data.body_html,
       votes: data.ups - data.downs,
       numberOfReplies: data.replies ? data.replies.data.children.length : 0,
       replies: data.replies || []
@@ -70,17 +74,16 @@ export const fetchSubredditData = async (subreddit: string) => {
   const url = `https://www.reddit.com/r/${subreddit}.json`;
   const response = await fetch(url);
   const json = await response.json();
-  return json.data.children.map(({data}: any) => {
-    return {
-      selftext: data.selftext,
-      title: data.title,
-      author: data.author,
-      votes: data.ups - data.downs,
-      url: data.url
-    }
-  })
+  console.log(json)
+  return transformJsonResponse(json);
 }
 
+export const fetchPopularSubreddits = async() => {
+  const url = 'https://www.reddit.com/subreddits/popular.json';
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.data.children.map(({ data } : any) => data.display_name_prefixed);
+}
 
 /* 
 fetch top, best, new, hot:
