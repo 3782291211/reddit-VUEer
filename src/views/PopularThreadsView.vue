c8786f0a199212bb635af79cd84728f1cffe691b
3782291211 <Laklouk98@hotmail.com>
2023-06-28 01:04:40 +0100

    Adding view for popular threads

---
+++ src/views/PopularThreadsView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PopularThreads from '../components/PopularThreads.vue'
+</script>
+
+<template>
+  <div class="about">
+    <PopularThreads/>
+  </div>
+</template>
+
+<style>
+</style>
