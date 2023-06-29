e48b5c258845523450068c2bc3185702cf227231
3782291211 <Laklouk98@hotmail.com>
2023-06-29 03:35:35 +0100

    Ading button functionality to nested replies

--- src/components/NestedReplies.vue
+++ src/components/NestedReplies.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import { formatHTML } from '@/utils/formatHTML';
+import { ref, reactive } from 'vue';
+import plus from '../assets/plus.vue';
 
 const props = defineProps(['replies']);
-onMounted(() => {
-   // console.log(props.replies)
-})
+const showReplies: { [key: string]: boolean } = reactive({ id: false });
+
+const toggleReplies = (id: string) => {
+  showReplies[id] = !showReplies[id];
+}
 
 </script>
 
 <template>
   <ul>
     <li v-for="reply in props.replies" :key="reply.data.id">
-        {{ console.log(reply) }}
-        <template v-if="reply.data.replies">
-            <NestedReplies :replies="reply.data.replies.data.children"/>
-        </template>
-        <div v-else>
-            <p class="nested-author">{{ reply.data.author }}</p>
+        <div v-if="reply.data.body_html" class="terminal-reply">
+            <div class="reply-header">
+              <plus :id="reply.data.id" v-if="reply.data.replies" @click="toggleReplies(reply.data.id)"/>
+              <p class="nested-author">{{ reply.data.author }}</p>
+            </div>
             <p v-html="formatHTML(reply.data.body_html)"></p>
         </div>
+        <template v-if="reply.data.replies && showReplies[reply.data.id]">
+            <NestedReplies :replies="reply.data.replies.data.children"/>
+        </template>
     </li>
   </ul>
 </template>
@@ -27,10 +32,27 @@ onMounted(() => {
 <style scoped>
 ul {
     list-style: none;
+    padding-left: 20px;
+    margin: 8px 0;
+}
+.terminal-reply {
+    padding-left: 10px;
+    margin-bottom: 10px
+}
+
+button {
+    margin-left: 10px;
+    opacity: 0;
+    color: white
 }
 
 .nested-author {
-    font-style: bold;
-    color: red
+    font-weight: 900;
+    display: inline-block;
+}
+
+.toggle-nested {
+    float: left;
+    margin-right: 4px;
 }
 </style>
