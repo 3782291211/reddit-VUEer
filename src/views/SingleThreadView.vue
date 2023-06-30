63e53f43a5f798155d2464c6d8ae95390adf6016
3782291211 <Laklouk98@hotmail.com>
2023-06-30 06:24:40 +0100

    Adding styles for single thread view

--- src/views/SingleThreadView.vue
+++ src/views/SingleThreadView.vue
@@ -22,28 +22,42 @@ onMounted(async () => {
 <template>
   <main v-if="!isLoading">
     <h1>{{ data.title }}</h1>
-    <h2>by {{ data.author }}, in {{ subreddit }}</h2>
+    <h2>by <span>{{ data.author }}</span>, in <span>{{ subreddit }}</span></h2>
     <!-- <p id="thread-body">{{ data.selftext }}</p> -->
-    <article v-html="formatHTML(data.selftext)"></article>
-    <h2>Comments</h2>
-    <ul>
-      <template v-for="item in data.comments" :key="item.id">
-        <li v-if="item.author">
-          <h3>By {{ item.author }}</h3>
-          <p class="comment-body" v-html="formatHTML(item.body)"></p>
-          <p>Votes: {{ isNaN(item.votes) ? '0' : item.votes }}</p>
-          <template v-if="item.numberOfReplies">
-            <p>Replies: {{ item.numberOfReplies }}</p>
-            <NestedReplies :replies="item.replies.data.children"/>
-          </template>
-        </li>
-      </template>
-    </ul>
+    <article v-if="data.selftext" class="thread-description" v-html="formatHTML(data.selftext)"></article>
+    <p v-else>This thread is missing a body section. Check back later!</p>
+    <section v-if="data.comments.length">
+      <h2>Comments</h2>
+      <ul>
+        <template v-for="item in data.comments" :key="item.id">
+          <li v-if="item.author">
+            <h3>{{ item.author }}</h3>
+            <div class="comment-body">
+              <article v-html="formatHTML(item.body)"></article>
+              <div class="flex">
+                <span class="inline"><img src="../assets/updown.svg">{{ isNaN(item.votes) ? '0' : item.votes }} votes</span>
+                <span v-if="item.numberOfReplies" class="float"><img src="../assets/bubble.svg">{{ item.numberOfReplies > 1 ? `${item.numberOfReplies} replies` : '1 reply' }}</span>
+              </div>
+            </div>
+            <template v-if="item.numberOfReplies">
+              <NestedReplies :replies="item.replies.data.children"/>
+            </template>
+          </li>
+        </template>
+      </ul>
+    </section>
+    <p class="no-comments" v-else>There aren't any comments for this thread.</p>
   </main>
   <p v-else>Loading article and comments...</p>
 </template>
 
 <style scoped>
+main {
+  width: 60%;
+  margin-left: 290px !important;
+  margin-top: 50px
+}
+
 h1 {
     margin-top: 40px;
 }
@@ -51,6 +65,38 @@ h1 {
 ul {
     list-style: none;
     padding: 0
+}
+
+h2 {
+  border-top: 1px solid gray;
+  padding: 4px 0 6px;
+  margin: 15px 0 25px;
+}
+
+span {
+  font-style: italic;
+  font-weight: 700;
+}
+
+img {
+  display: inline;
+  margin: -3px 4px
+}
+
+.float {
+  float: right;
+}
+
+.inline {
+  display: inline;
+  padding: 0;
+  margin-right: 15px
+}
+
+.flex {
+  border-top: 1px solid gray;
+  padding-top: 8px;
+  display: flex;
 }
 
 h3 {
@@ -65,20 +111,31 @@ li {
     border: 1px solid gray;
     margin: 20px auto;
     border-radius: 6px;
-    padding-bottom: 5px
+}
+
+.thread-description {
+  margin-bottom: 30px;
 }
 .comment-body {
-    padding: 10px
+    padding: 10px;
+    background-color: #15171c;
+    border-bottom: 1px solid rgba(128, 128, 128, 0.478);
+    border-radius: 7px;
+}
+.no-comments {
+  margin: 10px 0;
 }
 
 li p {
     padding: 0 10px
 }
 
-@media (min-width: 1000px) {
+@media (max-width: 1000px) {
   main {
-    margin-left: 160px;
-    margin-right: 160px
+    width: 95%;
+    margin: 100px auto 50px !important;
+    /* margin-left: 160px;
+    margin-right: 160px */
   }
 }
 </style>
