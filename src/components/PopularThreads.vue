6f3f6fb962f05159cebcb9c9fae1fbb20930c9cc
3782291211 <Laklouk98@hotmail.com>
2023-06-30 06:23:40 +0100

    Adjusting styles for popular threads page

--- src/components/PopularThreads.vue
+++ src/components/PopularThreads.vue
@@ -35,11 +35,11 @@ const handleClick = async (e: MouseEvent) => {
     <h1>Popular threads</h1>
     <!-- <img src="https://external-preview.redd.it/4pLnyfEgjuCe9UPDfsqK-10zbHyaEOrWUt0Sq3nltsk.jpg?auto=webp&amp;v=enabled&amp;s=869cc5fc8a11c09bf0ea82bb0f1dcd9e658e8e6c"> -->
     <div class="pagination">
-      <button @click="handleClick" name="next">Next</button>
-      <button v-if="pagination.countOffset > 25" @click="handleClick" name="previous">Previous</button>
+      <img v-if="pagination.countOffset > 25" @click="handleClick" name="previous" class="arrow" src="@/assets/left.svg"/>
+      <img @click="handleClick" name="next" class="arrow" src="@/assets/right.svg"/>
     </div>
     <ul>
-      <li v-for="item in data">
+      <li v-for="item in data" :key="item.id" class="thread-preview">
         <ThreadPreviewCard :item="item"/>
       </li>
     </ul>
@@ -49,22 +49,13 @@ const handleClick = async (e: MouseEvent) => {
 <style scoped>
 
 ul {
-  padding: 0
-}
-
-li {
-  list-style-type: none;
-  border: 1px solid white;
-  margin: 15px auto;
-  max-width: 900px;
-  padding: 10px;
-  padding-top: 5px;
-  border-radius: 8px;
-  background-color: rgb(31, 31, 31);
+  padding: 0;
 }
 
 h1 {
-  margin-left: 37px
+  width: 95%;
+  margin: 80px auto -20px;
+  font-weight: 900;
 }
 
 h2 {
@@ -73,17 +64,37 @@ h2 {
   color: whitesmoke
 }
 
-button[name="previous"] {
-  margin: 0 10px
+.arrow {
+  display: inline;
+  height: 45px;
+  width: 45px;
+  padding: 5px;
+  transition: padding 0.4s ease-in-out;
+  margin: 0 5px
 }
+
+.arrow:hover {
+  padding: 0;
+}
+
 .pagination {
-  margin: 20px 38px
+  text-align: center;
+  margin: 20px 0 -10px
 }
 
 @media (min-width: 1000px) {
-  main {
-  margin-left: 120px;
-  margin-right: 120px
-}
+   main {
+    margin-left: 120px;
+  }
+  li {
+    width: 70%
+  }
+  h1 {
+    width: 70%;
+    margin: 50px auto -20px;
+  }
+  .pagination {
+    margin: -10px 0
+  }
 }
 </style>
