dabb94d505fb0c9a93d268ba89221d8820e4ff3d
3782291211 <Laklouk98@hotmail.com>
2023-06-30 06:23:22 +0100

    Adjusting styles for nested replies

--- src/components/NestedReplies.vue
+++ src/components/NestedReplies.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { formatHTML } from '@/utils/formatHTML';
-import { ref, reactive } from 'vue';
+import { reactive } from 'vue';
 import plus from '../assets/plus.vue';
 
 const props = defineProps(['replies']);
@@ -17,10 +17,10 @@ const toggleReplies = (id: string) => {
     <li v-for="reply in props.replies" :key="reply.data.id">
         <div v-if="reply.data.body_html" class="terminal-reply">
             <div class="reply-header">
-              <plus :id="reply.data.id" v-if="reply.data.replies" @click="toggleReplies(reply.data.id)"/>
+              <plus v-if="reply.data.replies" :id="reply.data.id" @click="toggleReplies(reply.data.id)"/>
               <p class="nested-author">{{ reply.data.author }}</p>
             </div>
-            <p v-html="formatHTML(reply.data.body_html)"></p>
+            <article v-html="formatHTML(reply.data.body_html)"></article>
         </div>
         <template v-if="reply.data.replies && showReplies[reply.data.id]">
             <NestedReplies :replies="reply.data.replies.data.children"/>
@@ -40,15 +40,37 @@ ul {
     margin-bottom: 10px
 }
 
+/* Adjust spacing between nested comments */
+.reply-header {
+  margin: 12px 0 5px 2px;
+}
+.nested-author {
+  font-weight: 900;
+  display: inline-block;
+}
+
+ul {
+  margin: 0;
+}
+
+article {
+  background-color: #15171c;
+  margin-top: 2px;
+  margin-right: 10px;
+  margin-bottom: -2px;
+  padding: 7px 12px 1px;
+  border-radius: 8px;
+  border: 1px solid rgba(128, 128, 128, 0.296);
+}
+
+p {
+  margin: 0;
+}
+
 button {
     margin-left: 10px;
     opacity: 0;
     color: white
-}
-
-.nested-author {
-    font-weight: 900;
-    display: inline-block;
 }
 
 .toggle-nested {
