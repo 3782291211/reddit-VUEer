fc5cc1b749f83b91899ae8b4776927ebb8b2c672
3782291211 <Laklouk98@hotmail.com>
2023-07-01 04:24:28 +0100

    Adding font awesome imports

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-const app = createApp(App);
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faMagnifyingGlass, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
+/* add icons to the library */
+library.add(faMagnifyingGlass, faCircleArrowRight);
+
+const app = createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(router);
 
