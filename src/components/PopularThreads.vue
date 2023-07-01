3a72a80f677a27dc6cb567593f861c865fd000ee
3782291211 <Laklouk98@hotmail.com>
2023-07-01 04:23:23 +0100

    Adding feature: filter by new/best/top/popular/hot

--- src/components/PopularThreads.vue
+++ src/components/PopularThreads.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import type { Ref } from 'vue';
 import { fetchPopularThreads } from '../utils/apiRequests';
 import { paginate } from '../utils/eventHandlers';
 import ThreadPreviewCard from '../components/ThreadPreviewCard.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 const pagination: Ref<Pagination> = ref({
   afterQuery: null,
@@ -15,7 +18,7 @@ const pagination: Ref<Pagination> = ref({
 const data: Ref<Subreddit []> = ref([]);
 
 onMounted(async () => {
-  const response = await fetchPopularThreads([null, null, 0]);
+  const response = await fetchPopularThreads([null, null, 0, 'popular']);
   data.value = response.popularThreads;
   pagination.value = response.pagination;
 });
@@ -28,11 +31,24 @@ const handleClick = async (e: MouseEvent) => {
   }
 }
 
+watch(() => route.params.sortBy, async sortBy => {
+  const response = await fetchPopularThreads([
+    pagination.value.afterQuery, 
+    pagination.value.beforeQuery, 
+    pagination.value.countOffset,
+    sortBy as string
+  ]);
+  data.value = response.popularThreads;
+  pagination.value = response.pagination;
+  console.log(data.value);
+});
+
+
 </script>
 
 <template>
   <main>
-    <h1>Popular threads</h1>
+    <h1>{{ route.params.sortBy }} threads</h1>
     <!-- <img src="https://external-preview.redd.it/4pLnyfEgjuCe9UPDfsqK-10zbHyaEOrWUt0Sq3nltsk.jpg?auto=webp&amp;v=enabled&amp;s=869cc5fc8a11c09bf0ea82bb0f1dcd9e658e8e6c"> -->
     <div class="pagination">
       <img v-if="pagination.countOffset > 25" @click="handleClick" name="previous" class="arrow" src="@/assets/left.svg"/>
@@ -47,14 +63,21 @@ const handleClick = async (e: MouseEvent) => {
 </template>
 
 <style scoped>
+main {
+  position: relative;
+  top: 80px;
+}
 
 ul {
   padding: 0;
 }
 
 h1 {
-  width: 95%;
-  margin: 80px auto -20px;
+  /*width: 95%;
+   margin: 80px auto -20px; */
+  position: relative;
+  left: 30px;
+  margin: 0 auto 20px;
   font-weight: 900;
 }
 
@@ -69,7 +92,7 @@ h2 {
   height: 45px;
   width: 45px;
   padding: 5px;
-  transition: padding 0.4s ease-in-out;
+  transition: padding 0.2s ease-in-out;
   margin: 0 5px
 }
 
@@ -83,15 +106,18 @@ h2 {
 }
 
 @media (min-width: 1000px) {
-   main {
+  main {
     margin-left: 120px;
+    top: 110px
   }
   li {
     width: 70%
   }
   h1 {
     width: 70%;
-    margin: 50px auto -20px;
+  }
+  ul {
+    margin-left: 40px;
   }
   .pagination {
     margin: -10px 0
