2d198de133502d902188b6bdf28b860e8f98dc45
3782291211 <Laklouk98@hotmail.com>
2023-07-02 11:46:56 +0100

    Updating pagination handler to take account of sort query

--- src/utils/eventHandlers.ts
+++ src/utils/eventHandlers.ts
@@ -1,10 +1,16 @@
 import { fetchPopularThreads } from "./apiRequests";
 
-export const paginate = async (pagination: Pagination, e: MouseEvent): Promise<PopularThreadsResponse | null> => {
+export const paginate = async (pagination: Pagination, e: MouseEvent, sortBy: string): Promise<PopularThreadsResponse | null> => {
   const action: string = (e.target as HTMLButtonElement).name;
   if (action === 'previous' && pagination.countOffset <= 25) return null;
-  const args: PopularThreadsArgs = action === 'previous' ? 
-  [null, pagination.beforeQuery, pagination.countOffset] : [pagination.afterQuery, null, pagination.countOffset];
+ // const args = action === 'previous' ? [null, pagination.beforeQuery] : [pagination.afterQuery, null];
+  const args: PopularThreadsArgs = [
+    action === 'previous' ? null : pagination.afterQuery,
+    action === 'previous' ? pagination.beforeQuery : null,
+    pagination.countOffset, 
+    sortBy
+  ];
+
   const response = await fetchPopularThreads(args);
   let newCountOffset: number = 0;
   if (action === 'next') {
