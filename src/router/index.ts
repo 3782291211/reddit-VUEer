63c9624706819cc5d8bb9917f47fb9d80bc34e63
3782291211 <Laklouk98@hotmail.com>
2023-07-02 11:44:26 +0100

    Adding lazy-loaded component to path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SingleThreadView from '../views/SingleThreadView.vue'
 import SubredditView from '../views/SubredditView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:sortBy',
+      path: '/:sortBy?',
       name: 'popular',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
@@ -21,7 +20,7 @@ const router = createRouter({
     {
       path: '/r/:subreddit/comments/:threadId/:threadTitle',
       name: 'thread',
-      component: SingleThreadView
+      component: () => import('../views/SingleThreadView.vue')
     }
   ]
 })
