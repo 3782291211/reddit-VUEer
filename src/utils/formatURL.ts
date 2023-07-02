f55e3f22005df5caa6d0ba0b854783ecd708cd88
3782291211 <Laklouk98@hotmail.com>
2023-07-02 11:47:31 +0100

    Fixing error related to regex capturing groups

--- src/utils/formatURL.ts
+++ src/utils/formatURL.ts
@@ -1,5 +1,9 @@
 // Captures the URL fragmnet between https://www. and the second path
 export const formatURL = (url: string): string | null => {
   if (url.includes('redd.it') || url.includes('reddit.com')) return null;
-  return url.match(/htt(?:p|ps):\/\/(?:www.)?([^/]*\/[^/]*\/..)/)?.[1] || null;
+  return url.match(/^htt(?:p|ps):\/\/www\.([^/]+\/[^/]+\/.{5})/)?.[1] || null;
+  //return url.match(/htt(?:p|ps):\/\/(?:www.)?([^/]*\/[^/]{8})/)?.[1] || null;
 }
+
+// (?:) creates a non-capturing group
+// add ? to specify that a given character is optional
