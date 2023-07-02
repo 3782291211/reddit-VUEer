698c9a4770ffdd719b4d7f6dd864663c271054d6
3782291211 <Laklouk98@hotmail.com>
2023-07-02 11:43:27 +0100

    Extracting styles away from component

--- src/components/NestedReplies.vue
+++ src/components/NestedReplies.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { formatHTML } from '@/utils/formatHTML';
 import { reactive } from 'vue';
-import plus from '../assets/plus.vue';
+import plus from '../assets/icons/plus.vue';
 
 const props = defineProps(['replies']);
 const showReplies: { [key: string]: boolean } = reactive({ id: false });
@@ -29,52 +29,4 @@ const toggleReplies = (id: string) => {
   </ul>
 </template>
 
-<style scoped>
-ul {
-    list-style: none;
-    padding-left: 20px;
-    margin: 8px 0;
-}
-.terminal-reply {
-    padding-left: 10px;
-    margin-bottom: 10px
-}
-
-/* Adjust spacing between nested comments */
-.reply-header {
-  margin: 12px 0 5px 2px;
-}
-.nested-author {
-  font-weight: 900;
-  display: inline-block;
-}
-
-ul {
-  margin: 0;
-}
-
-article {
-  background-color: #15171c;
-  margin-top: 2px;
-  margin-right: 10px;
-  margin-bottom: -2px;
-  padding: 7px 12px 1px;
-  border-radius: 8px;
-  border: 1px solid rgba(128, 128, 128, 0.296);
-}
-
-p {
-  margin: 0;
-}
-
-button {
-    margin-left: 10px;
-    opacity: 0;
-    color: white
-}
-
-.toggle-nested {
-    float: left;
-    margin-right: 4px;
-}
-</style>
+<style lang="css" scoped src="../assets/css/nested-replies.css"></style>
