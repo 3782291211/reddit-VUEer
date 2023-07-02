6838a2797cd4e48663b9ebd8d06b552bf2a940d5
3782291211 <Laklouk98@hotmail.com>
2023-07-02 11:42:38 +0100

    Adding loading spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="spinner">
+      <font-awesome-icon icon="spinner" spin spin-reverse size="2xl" />
+    </div>
+</template>
+
+<style scoped>
+.spinner {
+    font-size: 50px;
+    position: fixed;
+    left: 55%;
+    top: 45%;
+    text-align: center;
+    margin-bottom: -40px
+}
+
+@media (max-width: 1000px) {
+  .spinner {
+    left: 39%;
+}
+}
+</style>
