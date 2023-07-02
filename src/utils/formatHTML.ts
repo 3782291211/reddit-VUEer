f57b12a40aa2cb08c96b4e1a50999445e6d840a7
3782291211 <Laklouk98@hotmail.com>
2023-07-02 11:48:17 +0100

    Adding rest parameter to formatHTML function

--- src/utils/formatHTML.ts
+++ src/utils/formatHTML.ts
@@ -1,7 +1,9 @@
 // This function unescapes the HTML entities within an HTML string
 
-export const formatHTML = (htmlString: string) => {
+export const formatHTML = (...htmlStrings: string[]) => {
   const element = document.createElement("div");
-  element.innerHTML = htmlString;
+  htmlStrings.forEach(htmlString => {
+    element.innerHTML += htmlString;
+  });
   return element.childNodes.length === 0 ? "" : element.childNodes[0].nodeValue;
 }
