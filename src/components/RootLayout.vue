7b5019d3bac656892a38f59ea22eac3da754e222
3782291211 <Laklouk98@hotmail.com>
2023-07-03 19:51:41 +0100

    Adding active styles to router links

--- src/components/RootLayout.vue
+++ src/components/RootLayout.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, ref, Ref, StyleValue, watch } from 'vue';
+import { computed, onMounted, onUnmounted, ref, Ref, watch } from 'vue';
 import { searchSubreddits } from '@/utils/apiRequests';
 import { fetchPopularSubreddits } from '../utils/apiRequests';
 import { useRoute } from 'vue-router';
-import list from '../assets/list.vue';
-import close from '../assets/close.vue';
+import list from '../assets/icons/list.vue';
+import close from '../assets/icons/close.vue';
 import SearchBar from '../components/SearchBar.vue';
 
 const searchResults: Ref<string[]> = ref([]);
@@ -15,7 +15,6 @@ const inputIsBlurred: Ref<boolean> = ref(true);
 const activeParam: Ref<string> = ref('');
 const showMenu = ref(false);
 const windowWidth = ref(window.innerWidth);
-const sidebarZIndex = ref(1);
 const route = useRoute();
 
 onMounted(async () => {
@@ -33,7 +32,8 @@ watch([searchTerm, inputIsBlurred], async ([newSearchTerm]) => {
 });
 
 watch(() => route.params, toParam => {
-  activeParam.value = toParam.subreddit as string || '';
+  activeParam.value = toParam.subreddit as string || toParam.sortBy as string;
+  showMenu.value = false;
 })
 
 const noResults = computed(() => {
@@ -44,19 +44,20 @@ const noResults = computed(() => {
 
 const showSearchResults = computed(() => {
   return searchResults.value.length && !inputIsBlurred.value;
-})
+});
+
+const activeClass = (urlPathFragment: string) => {
+  return [{ 
+    currentSubreddit : urlPathFragment.slice(2) === activeParam.value,
+    currentSort: urlPathFragment === activeParam.value
+  }];
+}
 
 const handleBlur = (e: FocusEvent) => {
   const targetName = (e.relatedTarget as any)?.name;
   if (targetName === "link") return;
   inputIsBlurred.value = true;
 }
-
-/*const toggleZIndex = (e: boolean) => {
-  console.log(e);
-  if (e) sidebarZIndex.value = 0;
-  sidebarZIndex.value = 1;
-}*/
 
 const handleFocus = () => inputIsBlurred.value = false;
 const handleWidthChange = () => windowWidth.value = window.innerWidth;
@@ -65,35 +66,34 @@ const roundTerminalBorders = (sub: string, resultsArray: string[]) => {
   if (resultsArray.indexOf(sub) === resultsArray.length - 1) return 'last';
 }
 
-// const headerPosition = computed(() => ({ position: showMenu ? 'static' : 'fixed' }));
-
 </script>
 
 <template>
   <header>
     <h2 class="banner">reddit VUEer</h2>
-    <!-- <SearchBar @showing-results="toggleZIndex"/> -->
-    <SearchBar/>
+    <SearchBar v-if="windowWidth > 1000"/>
   </header>
   <div class="menu-controls">
     <list v-if="!showMenu" class="mobile" @click="() => showMenu = true"/>
     <close v-if="showMenu" class="mobile" @click="() => showMenu = false"/>
   </div>
   <section v-if="showMenu || windowWidth > 1000" id="feeds">
-    <h3>Filter threads</h3>
-    <div class="router-links">
-      <router-link class="grow" to="/popular">Popular</router-link>
-      <router-link class="grow" to="/top">Top</router-link>
-      <router-link to="/hot">Hot</router-link>
-      <router-link to="/new">New</router-link>
-      <router-link to="/best">Best</router-link>
+    <h3 class="filter">Filter threads</h3>
+    <div class="router-links" id="router-links">
+      <router-link :class="[...activeClass('popular'), 'grow']" to="/popular">Popular</router-link>
+      <router-link :class="[...activeClass('top'), 'grow']" to="/top">Top</router-link>
+      <router-link :class="activeClass('hot')" to="/hot">Hot</router-link>
+      <router-link :class="activeClass('new')" to="/new">New</router-link>
+      <router-link :class="activeClass('best')" to="/best">Best</router-link>
     </div>
-    <label for="search">Search subreddits</label>  
+    <label v-if="windowWidth < 1000">Search threads</label> 
+    <SearchBar v-if="windowWidth < 1000" :class="{mobileSearchbar: windowWidth < 1000}"/>
+    <label v-if="windowWidth < 1000" class="search-users">Search users</label>  
+    <label for="search" class="search-subreddits">Search subreddits</label>  
     <div class="flex">
       <input id="search" v-model.trim="searchTerm" @blur="handleBlur" @focus="handleFocus">
       <font-awesome-icon class="icon" :icon="['fas', 'magnifying-glass']" beat />
     </div>
-    <!-- <ul name="search-results" v-if="showSearchResults"> -->
     <ul name="search-results" v-if="showSearchResults">
       <li v-for="subreddit in searchResults" :key="subreddit">
         <a name="link" :href="`/${subreddit}`"><p :class="[roundTerminalBorders(subreddit, searchResults), 'search-results']">{{ subreddit }}</p></a>
@@ -102,13 +102,14 @@ const roundTerminalBorders = (sub: string, resultsArray: string[]) => {
     <p class="no-results" v-else-if="noResults">No results.</p>
     <template v-if="!showSearchResults">
     <h3>Popular subreddits</h3>
-      <ul name="popular-subreddits">
+      <ul v-if="popularSubreddits.length" name="popular-subreddits">
         <li v-for="subreddit in popularSubreddits" :key="subreddit">
-          <router-link :class="[{ current : subreddit.slice(2) === activeParam }, 'popular']" :to="`/${subreddit}`">{{ subreddit }}</router-link>
+          <router-link :class="[...activeClass(subreddit), 'popular']" :to="`/${subreddit}`">{{ subreddit }}</router-link>
         </li>
       </ul>
+      <font-awesome-icon v-else icon="spinner" spin transform="right-90% up-19" spin-reverse size="2xl" />
     </template>
   </section>
 </template>
 
-<style lang="css" scoped src="../assets/root-layout.css"/>
+<style lang="css" scoped src="../assets/css/root-layout.css"/>
