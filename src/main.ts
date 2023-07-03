74163acf1d56c08c10af690c99e6583f37ee0d13
3782291211 <Laklouk98@hotmail.com>
2023-07-03 19:49:17 +0100

    Importing font awesome icons

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import { faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faSpinner, faUser } from '@fortawesome/free-solid-svg-icons';
 /* add icons to the library */
-library.add(faMagnifyingGlass, faSpinner);
+library.add(faMagnifyingGlass, faSpinner, faUser);
 
 const app = createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon);
