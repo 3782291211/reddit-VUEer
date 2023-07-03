b292125d0b94d7af4b07aa02e5b858a99973972f
3782291211 <Laklouk98@hotmail.com>
2023-07-03 19:52:21 +0100

    Emitting event from reusable pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+defineProps<{
+    pagination: Pagination
+}>();
+
+</script>
+
+<template>
+  <div class="pagination">
+    <img v-if="pagination.countOffset > 25" @click="$emit('handlePagination', $event)" name="previous" class="arrow" src="@/assets/images/left.svg"/>
+    <img @click="$emit('handlePagination', $event)" name="next" class="arrow" src="@/assets/images/right.svg"/>
+  </div>
+</template>
+
+<style lang="css" scoped src="../assets/css/popular-threads.css"></style>
