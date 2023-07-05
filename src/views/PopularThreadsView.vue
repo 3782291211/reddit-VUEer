e844518d340acead6dff3d7f84f70be295610e3a
3782291211 <Laklouk98@hotmail.com>
2023-07-05 06:50:11 +0100

    Adding trending threads component to outlet

--- src/views/PopularThreadsView.vue
+++ src/views/PopularThreadsView.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import PopularThreads from '@/components/PopularThreads.vue';
+import Trending from '@/components/Trending.vue';
 
 </script>
 
 <template>
+  <Trending/>
   <PopularThreads view-type="home"/>
 </template>
