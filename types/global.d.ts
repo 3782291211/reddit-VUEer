c2f1869c3b84fea29e1238d5d4cb116f746f9fd8
3782291211 <Laklouk98@hotmail.com>
2023-07-06 00:24:09 +0100

    Adding global types for API functions

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,20 +3,84 @@ export {}
 declare global {
   type stringOrNull = string | null;
 
-  export type Subreddit = {
-    name: string,
-    [key: string]: any
-  };
-
   export type ThreadPreviewProps = {
     item: Subreddit
   }
 
-  export type PopularJsonResponse = { 
-    data: {
-      [key: string]: any 
+  export type ErrorJson = {
+    reason: string,
+    error: number,
+    message: string
+  }
+
+  /* JSON data structure common to most response types from the API */
+  type BaseDataStructure = {
+    after: stringOrNull,
+    before: stringOrNull,
+    dist: number | null,
+    geo_filter: string,
+    modhash: string
+  }
+
+  /* JSON response returnd by initial fetch in fetchPopularThreads */
+  export type PopularThreadsJson = { 
+    data: BaseDataStructure & {
+      children: ThreadJson[],
     }, 
     kind: string 
+  }
+
+  export type ThreadJson = {
+    kind: string,
+    data: {
+      id: string,
+      subreddit: string,
+      permalink: string,
+      selftext_html: stringOrNull,
+      title: string,
+      author: string,
+      ups: number,
+      downs: number,
+      thumbnail: string,
+      url: string,
+      secure_media: {
+      //   oembed: {
+      //     description: string,
+      //     html: string,
+      //     provider_url: string,
+      //     title: string,
+      //     [key: string] : any
+      //   },
+      //   type: string
+      // } | {
+        reddit_video: {
+          fallback_url: string,
+          [key: string]: any
+        }
+      } | null
+    }
+  }
+
+  /* An array of formatted Thread objects is provided to the view */
+  export type Thread = {
+    id: string,
+    subreddit: string,
+    url: string,
+    selftext: stringOrNull,
+    title: string,
+    author: string,
+    ups: number,
+    downs: number,
+    thumbnail: string,
+    src: string,
+    media: stringOrNull
+  };
+  
+  export type PopularThreadsArgs = [stringOrNull, stringOrNull, number, stringOrNull, stringOrNull];
+  
+  export type PopularThreadsResponse = {
+    popularThreads: Thread[],
+    pagination : Pagination
   }
 
   export type Pagination = {
@@ -25,13 +89,214 @@ declare global {
     countOffset: number
   }
 
-  export type PopularThreadsArgs = [stringOrNull, stringOrNull, number, stringOrNull, stringOrNull];
-  export type SearchUserPosts = [stringOrNull, stringOrNull, number, string];
-
-  export type PopularThreadsResponse = {
-    popularThreads: Subreddit[],
-    pagination : Pagination
+  ////////////////////////////////////////////////////////////////////////
+  /* Single thread JSON response */
+  type ThreadBodyJson = {
+    kind: string,
+    data: BaseDataStructure & {
+      children: [
+        {
+          kind: string,
+          data: {
+            author: string,
+            title: string,
+            ups: number,
+            downs: number,
+            selftext_html: stringOrNull,
+            subreddit_name_prefixed: string,
+            url: string,
+            secure_media: {
+              reddit_video: {
+                fallback_url: string,
+                [key: string]: any
+              }
+            } | null,
+            secure_media_embed: {
+              content: string,
+              [key: string]: any
+            } | {},
+            media_metadata?: {
+              [key: string]: {
+                status: string,
+                e: string,
+                id: string,
+                m: string,
+                p: {
+                  u: string,
+                  x: number,
+                  y: number
+                }[],
+                s: {
+                  u: string,
+                  x: number,
+                  y: number
+                }
+              }
+            },
+            preview: {
+              enabled: boolean,
+              images: [{
+                source: {
+                  url: string,
+                  height: number,
+                  width: number
+                },
+                [key: string]: any
+              }];
+            }
+          }
+        }
+      ]
+    }
   }
+
+  export type Comment = {
+    kind: string,
+    data: {
+      id: string,
+      author: string,
+      body: stringOrNull,
+      ups: number,
+      downs: number,
+      replies: {
+        kind: string,
+        data: BaseDataStructure & {
+          children: (Comment | unknown)[]
+        }
+      } | "",
+    }
+  }
+
+  /* This array is what we receive in the API JSON response */
+  export type SingleThreadJson = [
+    ThreadBodyJson,
+    {
+      kind: string,
+      data: BaseDataStructure & {
+        children: Comment[]
+      }
+    }
+  ]
+
+  /* We format (map) the JSON comments array before we return it to the view */
+  export type FormattedComment = {
+    id: number,
+    author: string,
+    body: string,
+    votes: number,
+    numberOfReplies: number,
+    replies: {
+      kind: string,
+      data: BaseDataStructure & {
+        children: (Comment | unknown)[]
+      }
+    } | []
+  }
+
+  /* This is the formatted data we send to the view */ 
+  export type SingleThreadResponse = {
+    author: string,
+    title: string,
+    votes: number,
+    comments: FormattedComment[],
+    selftext: stringOrNull,
+    sub: string,
+    url: string,
+    media: {
+      reddit_video: {
+        fallback_url: string,
+        [key: string]: any
+      }
+    } | null,
+    images: unknown,
+    preview: string,
+    embed: string
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  /* Search subreddits */
+  export type SubredditJson = {
+    kind: string,
+    data: {
+      display_name_prefixed: string,
+      [Key: string]: any
+    }
+  }
+
+  export type SearchSubredditsJson = {
+    kind: string,
+    data: BaseDataStructure & {
+      children: SubredditJson[]
+    }
+  } | Record<string, never>;
+
+  export type PopularSubredditsJson = SearchSubredditsJson;
+
+  ////////////////////////////////////////////////////////////////////////////
+  /* Search threads */
+  export type SearchThreadsJson = { 
+    data: BaseDataStructure & {
+      children: ThreadJson[] | [],
+      facets: Record<string, never>
+    }, 
+    kind: string 
+  }
+
+  /* This is the data being sent to the view */
+  export type SearchThreadsResponse = {
+    id: string,
+    title: string,
+    link: string
+  }[];
+
+   ////////////////////////////////////////////////////////////////////////////
+  /* Subreddit body */
+  export type SubredditBodyJson = {
+    kind: string,
+    data: {
+      active_user_count: number,
+      description_html: stringOrNull,
+      public_description_html: string,
+      advertiser_category: string,
+      banner_img: stringOrNull,
+      header_img: stringOrNull,
+      icon_img: stringOrNull
+    }
+  }
+
+   /* This is the data being sent to the view */
+   export type SubredditBodyResponse = {
+     subscribers: number,
+     description: stringOrNull,
+     publicDescription: string,
+     category: string,
+     banner: stringOrNull,
+     header: stringOrNull,
+     icon: stringOrNull
+   }
+  
+
+  ///////////////////////////////////////////////////////////////////////////
+  /* Search user */
+  export type SearchUserJson = [
+    {
+      kind: string,
+      data: BaseDataStructure & {
+        children: {
+            kind: string,
+            data: {
+              icon_img: 
+            }
+          }[]
+      }
+    },
+    {
+      kind: string,
+      data: {}
+    }
+  ]
+
+  export type SearchUserPosts = [stringOrNull, stringOrNull, number, string];
 
   export type Post = {
     id: string,
@@ -90,4 +355,17 @@ declare global {
     data: { 
       [key: string]: any }
     };
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  /* Subreddit view */
+  export type SubredditBody = {
+    banner: string,
+    category: string,
+    description: stringOrNull,
+    header: string,
+    headerImg: string,
+    icon: string,
+    publicDescription: stringOrNull,
+    subscribers: number
+  };
 }
