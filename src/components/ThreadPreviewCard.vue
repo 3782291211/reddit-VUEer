369841edde4a0361a803f912b909ed5904ff65d7
3782291211 <Laklouk98@hotmail.com>
2023-07-05 06:47:27 +0100

    Adding router link to preview card

--- src/components/ThreadPreviewCard.vue
+++ src/components/ThreadPreviewCard.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 
 import { computed, ref } from 'vue';
-import { useRoute } from 'vue-router';
 import { formatHTML } from '../utils/formatHTML';
 import { formatURL } from '../utils/formatURL';
 
 const { item } = defineProps<{ item: Subreddit }>();
-const route = useRoute();
 
 const showImage = ref(true);
 const showThumbnail = ref(true);
@@ -24,10 +22,10 @@ const thumbnailSrc = computed(() => {
     <div class="author-votes">
       <p class="subreddit">
         By 
-        <router-link class="user-link" :to="`/search/${item.author}`">{{ item.author }}</router-link>
+        <!-- <router-link class="user-link" :to="`/search/${item.author}`">{{ item.author }}</router-link> -->
+        <router-link class="user-link" :to="{ path: '/search', query: { username: item.author } }">{{ item.author }}</router-link>
         in 
-        <router-link v-if="!route.params.subreddit" class="subreddit-link" :to="item.subreddit">{{ item.subreddit }}</router-link>
-        <span class="orange" v-else>{{ item.subreddit }}</span>
+        <router-link class="subreddit-link" :to="{name: 'subreddit', params: { subreddit: item.subreddit.slice(2) }}">{{ item.subreddit }}</router-link>
       </p>
       <div class="votes">
         <img src="../assets/icons/updown.svg">
@@ -35,15 +33,15 @@ const thumbnailSrc = computed(() => {
       </div>
     </div>
     <a class="external" v-if="formatURL(item.src)" :href="item.src" target="_blank">{{ formatURL(item.src) + '...' }}</a>
-    <router-link :to="item.url">
+    <router-link class="relative" :to="item.url">
       <div id="v-html" v-if="item.selftext" v-html="formatHTML(item.selftext)"></div>
       <img v-if="showImage" referrerpolicy="no-referrer" :src="item.src" @error="() => showImage = false"/>
       <img v-if="thumbnailSrc" :src="item.thumbnail" @error="() => showThumbnail = false" style="display: inline"/>
     </router-link>
-    <video v-if="item.media" width="400" height="300" controls>
-      <source :src="item.media" type="video/mp4">
-      Your browser does not support this file format.
-    </video>
+      <video v-if="item.media" width="400" height="300" controls>
+        <source :src="item.media" type="video/mp4">
+        Your browser does not support this file format.
+      </video>
   </article>
 </template>
 
