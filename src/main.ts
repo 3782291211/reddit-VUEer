4c133dc4c08f448a8d82993a7fc46e68a501f9f1
3782291211 <Laklouk98@hotmail.com>
2023-07-05 06:51:01 +0100

    Adding font awesome icons to library

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import { faMagnifyingGlass, faSpinner, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleLeft, faArrowAltCircleRight, faChevronCircleLeft, faChevronCircleRight, faCircleArrowLeft, faCircleNotch, faHeartMusicCameraBolt, faMagnifyingGlass, faPauseCircle, faPlayCircle, faSpinner, faUser } from '@fortawesome/free-solid-svg-icons';
 /* add icons to the library */
-library.add(faMagnifyingGlass, faSpinner, faUser);
+library.add(faMagnifyingGlass, faSpinner, faUser, faHeartMusicCameraBolt, faChevronCircleLeft, faChevronCircleRight, faPauseCircle, faPlayCircle, faCircleNotch);
 
 const app = createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon);
