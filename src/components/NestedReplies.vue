3555a2bad1846b4663e52fcd9b9326587b729e82
3782291211 <Laklouk98@hotmail.com>
2023-07-05 06:45:53 +0100

    Adding reduce function to compute no. of nested replies

--- src/components/NestedReplies.vue
+++ src/components/NestedReplies.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { formatHTML } from '@/utils/formatHTML';
-import { reactive } from 'vue';
-import plus from '../assets/icons/plus.vue';
+import { computed, reactive } from 'vue';
+import plusOrMinus from '../assets/icons/plusOrMinus.vue';
 
 const props = defineProps(['replies']);
 const showReplies: { [key: string]: boolean } = reactive({ id: false });
@@ -10,21 +10,32 @@ const toggleReplies = (id: string) => {
   showReplies[id] = !showReplies[id];
 }
 
+const showNestedReplies = (reply: any) => {
+  return reply.data.replies
+    && reply.data.replies.data.children[0].kind !== 'more'
+    && showReplies[reply.data.id];
+}
+
+const showExpandButton = (reply: any) => {
+  return reply.data.replies
+    && reply.data.replies.data.children[0].kind !== 'more';
+}
+
 </script>
 
 <template>
   <ul>
     <li v-for="reply in props.replies" :key="reply.data.id">
-        <div v-if="reply.data.body_html" class="terminal-reply">
-            <div class="reply-header">
-              <plus v-if="reply.data.replies" :id="reply.data.id" @click="toggleReplies(reply.data.id)"/>
-              <p class="nested-author">{{ reply.data.author }}</p>
-            </div>
-            <article v-html="formatHTML(reply.data.body_html)"></article>
+      <div v-if="reply.data.body_html" class="terminal-reply">
+        <div class="reply-header">
+          <plusOrMinus v-if="showExpandButton(reply)" :id="reply.data.id" @click="toggleReplies(reply.data.id)" :shape="showReplies[reply.data.id] ? 'minus' : 'plus'"/>
+          <p class="nested-author">{{ reply.data.author }}</p>
         </div>
-        <template v-if="reply.data.replies && showReplies[reply.data.id]">
-            <NestedReplies :replies="reply.data.replies.data.children"/>
-        </template>
+        <article v-html="formatHTML(reply.data.body_html)"></article>
+      </div>
+      <template v-if="showNestedReplies(reply)">
+        <NestedReplies :replies="reply.data.replies.data.children"/>
+      </template>
     </li>
   </ul>
 </template>
