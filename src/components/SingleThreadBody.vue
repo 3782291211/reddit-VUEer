3cbe622f6feb5ecc4730e10bb83da268f4263eb5
3782291211 <Laklouk98@hotmail.com>
2023-07-06 03:22:46 +0100

    Adding image carousel with buttons

--- src/components/SingleThreadBody.vue
+++ src/components/SingleThreadBody.vue
@@ -2,10 +2,17 @@
 import { formatURL } from '../utils/formatURL';
 import { formatHTML } from '@/utils/formatHTML';
 import { removeEntities } from '@/utils/removeEntities';
+import{ computed, ref } from 'vue';
 
-defineProps<{
-    data: any
+const props = defineProps<{
+    data: SingleThreadResponse
 }>();
+
+const imageIndex = ref(0);
+const currentIndex = computed(() => {
+  return props.data.images[Math.abs(imageIndex.value) % props.data.images.length]
+});
+
 </script>
 
 <template>
@@ -22,19 +29,31 @@ defineProps<{
     <h3 v-if="data.selftext" class="op">Original post</h3>
     <article v-if="data.selftext" class="thread-description" v-html="formatHTML(data.selftext)"></article>
     <a v-if="formatURL(data.url)" :href="data.url" target="_blank" class="external">{{ formatURL(data.url) }}...</a>
-
-    <div v-if="data.preview" class="caption">
+    
+    <div v-if="data.preview || data.url" class="caption">
       <p v-if="data.preview && data.media?.reddit_video">Preview</p>
-      <a :href="data.url" target="_blank"><img class="preview-img" :src="removeEntities(data.preview)"></a>
+      <a :href="data.url" target="_blank">
+        <div class="image-bg" :style="{ backgroundImage: `url(${removeEntities(data.preview) || data.url})`}">
+          <img class="preview-img" :src="removeEntities(data.preview)" @error="e => (e.target as HTMLImageElement).src = data.url">
+        </div>
+      </a>
     </div>
     
     <div class="embed" v-if="data.embed" v-html="formatHTML(data.embed)"></div>
 
-    <ul v-if="data.images.length">
-      <li v-for="image in data.images" :key="image" name="gallery-img">
-        <img class="gallery-img" :src="removeEntities(image)" referrerpolicy="origin" @error="() => console.log('yayaya')">
-      </li>
-    </ul>
+    <figure v-if="data.images.length">
+      <div class="image-bg" :style="{ backgroundImage: `url(${removeEntities(currentIndex)})`}">
+        <img class="gallery-img" :src="removeEntities(currentIndex)" referrerpolicy="origin">
+      </div>
+      <div class="gallery-buttons">
+        <span name="previous">
+          <font-awesome-icon @click="() => imageIndex--" icon="fa-solid fa-circle-chevron-left"/>
+        </span>
+        <span name=next>
+          <font-awesome-icon @click="() => imageIndex++" icon="fa-solid fa-circle-chevron-right"/>
+        </span>
+      </div>
+    </figure>
       
     <video v-if="data.media?.reddit_video" controls>
       <source :src="data.media?.reddit_video.fallback_url" type="video/mp4">
