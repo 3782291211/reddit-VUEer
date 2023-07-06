4966bbeaad19d13041441cd83c56417c58440b37
3782291211 <Laklouk98@hotmail.com>
2023-07-06 09:48:49 +0100

    Uploading config and preparing application for deployment.

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import { faArrowAltCircleLeft, faArrowAltCircleRight, faChevronCircleLeft, faChevronCircleRight, faCircleArrowLeft, faCircleNotch, faHeartMusicCameraBolt, faMagnifyingGlass, faPauseCircle, faPlayCircle, faSpinner, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faChevronCircleLeft, faChevronCircleRight, faCircleNotch, faHeartMusicCameraBolt, faMagnifyingGlass, faPauseCircle, faPlayCircle, faSpinner, faUser, faCommenting, faFileCirclePlus, faFaceFlushed } from '@fortawesome/free-solid-svg-icons';
 /* add icons to the library */
-library.add(faMagnifyingGlass, faSpinner, faUser, faHeartMusicCameraBolt, faChevronCircleLeft, faChevronCircleRight, faPauseCircle, faPlayCircle, faCircleNotch);
+library.add(faMagnifyingGlass, faSpinner, faUser, faHeartMusicCameraBolt, faChevronCircleLeft, faChevronCircleRight, faPauseCircle, faPlayCircle, faCircleNotch, faCommenting, faFileCirclePlus, faFaceFlushed);
 
 const app = createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon);
