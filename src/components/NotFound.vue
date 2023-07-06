4d7fd76bec2ab43d93999b8d5efe0dca1fd3dfea
3782291211 <Laklouk98@hotmail.com>
2023-07-06 09:34:56 +0100

    Adding 404 catch all route

---
+++ src/components/NotFound.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Not found.</h1>
+    <font-awesome-icon icon="fa-solid fa-face-flushed" shake />
+    <router-link class="not-found" :to="{path: '/'}">Return to home.</router-link>
+  </main>
+</template>
+
+<style scoped>
+main {
+    width: 50%;
+    text-align: center;
+    margin: 0 auto;
+    position: relative;
+    top: 30vh
+}
+
+a {
+    width: fit-content;
+    margin: 10px auto;
+    padding: 4px 8px;
+    border-radius: 7px
+}
+
+svg {
+    height: 70px;
+    margin: 10px;
+    color: indianred;
+}
+
+h1 {
+    margin-bottom: 15px
+}
+
+@media (max-width: 500px) {
+    main {
+        top: 20vh;
+        width: 100%
+    }
+}
+</style>
